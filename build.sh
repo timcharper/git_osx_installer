0a4e94c5f003c441019da12a61800f8c9daf8116
Tim Harper <timcharper@gmail.com>
2009-06-11 22:43:44 -0600

    incorporate some of the changes from club juggler

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 if [ "`uname`" == "Darwin" ]; then sed_regexp="-E"; else sed_regexp="-r"; fi 
 GIT_VERSION="${1:-`curl http://git-scm.com/ 2>&1 | grep "<div id=\"ver\">" | sed $sed_regexp 's/^.+>v([0-9.]+)<.+$/\1/'`}"
+PREFIX=/usr/local/git
+# Undefine to not use sudo
+SUDO=sudo
 
 echo "Building GIT_VERSION $GIT_VERSION"
 
-sudo mv /usr/local/git{,_`date +%s`}
-sudo UserScripts/cplibs.sh
+$SUDO mv $PREFIX{,_`date +%s`}
 
 mkdir -p git_build
 
@@ -18,27 +20,28 @@ pushd git_build
         # If you're on PPC, you may need to uncomment this line: 
         # echo "MOZILLA_SHA1=1" >> Makefile_tmp
 
-        # Tell make to use /usr/local/git/lib rather than MacPorts:
+        # Tell make to use $PREFIX/lib rather than MacPorts:
         echo "NO_DARWIN_PORTS=1" >> Makefile_tmp
         cat Makefile >> Makefile_tmp
         mv Makefile_tmp Makefile
 
-        make LDFLAGS="-L/usr/local/git/lib,/usr/lib" prefix=/usr/local/git all
-        make LDFLAGS="-L/usr/local/git/lib,/usr/lib" prefix=/usr/local/git strip
-        sudo make LDFLAGS="-L/usr/local/git/lib,/usr/lib" prefix=/usr/local/git install
+	# Make fat binaries with ppc/32 bit/64 bit
+        make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" prefix=$PREFIX all
+        make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" prefix=$PREFIX strip
+        $SUDO make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386" prefix=$PREFIX install
 
         # contrib
-        sudo mkdir -p /usr/local/git/contrib/completion
-        sudo cp contrib/completion/git-completion.bash /usr/local/git/contrib/completion/
+        $SUDO mkdir -p $PREFIX/contrib/completion
+        $SUDO cp contrib/completion/git-completion.bash $PREFIX/contrib/completion/
     popd
     
     [ ! -f git-manpages-$GIT_VERSION.tar.bz2 ] && curl -O http://www.kernel.org/pub/software/scm/git/git-manpages-$GIT_VERSION.tar.bz2
-    sudo mkdir -p /usr/local/git/man
-    sudo tar xjvo -C /usr/local/git/man -f git-manpages-$GIT_VERSION.tar.bz2
+    $SUDO mkdir -p $PREFIX/man
+    $SUDO tar xjvo -C $PREFIX/man -f git-manpages-$GIT_VERSION.tar.bz2
 popd
 
 # change hardlinks for symlinks
-sudo ruby UserScripts/symlink_git_hardlinks.rb
+$SUDO ruby UserScripts/symlink_git_hardlinks.rb
 
 # add .DS_Store to default ignore for new repositories
-sudo sh -c "echo .DS_Store >> /usr/local/git/share/git-core/templates/info/exclude"
+$SUDO sh -c "echo .DS_Store >> $PREFIX/share/git-core/templates/info/exclude"
