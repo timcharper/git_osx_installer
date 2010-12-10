9f1b9d7acfd1b5eaca9e0fdb59ab8e100408fdc2
Tim Harper <timcharper@gmail.com>
2010-12-09 17:14:28 -0700

    It is redundant to say 'Intel'. i386 and x86_64 suffices.

--- build_package_test_and_bundle.sh
+++ build_package_test_and_bundle.sh
@@ -7,7 +7,7 @@ for ARCH in i386 x86_64; do
   ARCH=$ARCH ./build.sh
 
   GIT_VERSION=$(git --version | sed 's/git version //')
-  PACKAGE_NAME="git-$GIT_VERSION-intel-$ARCH-leopard"
+  PACKAGE_NAME="git-$GIT_VERSION-$ARCH-leopard"
   echo $PACKAGE_NAME | pbcopy
 
   echo "Git version is $GIT_VERSION"
@@ -17,7 +17,8 @@ for ARCH in i386 x86_64; do
 
   UNCOMPRESSED_IMAGE_FILENAME="$PACKAGE_NAME.uncompressed.dmg"
   IMAGE_FILENAME="$PACKAGE_NAME.dmg" 
-  hdiutil create $UNCOMPRESSED_IMAGE_FILENAME -srcfolder "Disk Image" -volname "Git $GIT_VERSION Intel $ARCH Leopard" -ov
+  rm -f $UNCOMPRESSED_IMAGE_FILENAME $IMAGE_FILENAME
+  hdiutil create $UNCOMPRESSED_IMAGE_FILENAME -srcfolder "Disk Image" -volname "Git $GIT_VERSION $ARCH Leopard" -ov
   hdiutil convert -format UDZO -o $IMAGE_FILENAME $UNCOMPRESSED_IMAGE_FILENAME
   rm $UNCOMPRESSED_IMAGE_FILENAME
 done
@@ -26,7 +27,7 @@ echo "Testing the $ARCH installer..."
 
 . test_installer.sh
 
-echo "Git Installer $GIT_VERSION - OS X - Leopard - Intel $ARCH" | pbcopy
+echo "Git Installer $GIT_VERSION - OS X - Leopard - $ARCH" | pbcopy
 open "http://code.google.com/p/git-osx-installer/downloads/entry"
 sleep 1
 open "./"
