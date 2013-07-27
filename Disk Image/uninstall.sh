6b651f327796b2eb181e2fbe1cffe1562c90c2bf
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-07-27 12:39:50 +0100

    pkgutil now correctly forgets receipts

--- Disk Image/uninstall.sh
+++ Disk Image/uninstall.sh
@@ -9,7 +9,7 @@ if [ "$response" == "yes" ]; then
   sudo rm -rf /usr/local/git/
   sudo rm /etc/paths.d/git
   sudo rm /etc/manpaths.d/git
-  sudo pkgutil --forget --pkgs=GitOSX\.Installer\.git[A-Za-z0-9]*\.[a-z]*.pkg
+  pkgutil --packages | grep GitOSX.Installer | xargs -I {} sudo pkgutil --forget {}
   echo "Uninstalled"
 else
   echo "Aborted"
